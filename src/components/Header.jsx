c36673944f4e08c8f43682146df58b80d1e2204e
aravindmt2020@gmail.com <aravindmt2020@gmail.com>
2025-01-10 14:00:49 +0530

    second commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return  (
+       <div className="container">
+        
+         <nav className="sticky top-0 bg-black text-red">
+            
+            </nav>
+       </div>
+    )
+}
