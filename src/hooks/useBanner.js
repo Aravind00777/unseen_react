5719f703210f91ebfbd72066894db5fa41afaac3
Aravind00777 <aravindmt2020@gmail.com>
2026-02-21 11:01:38 +0530

    Updates

--- src/hooks/useBanner.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import BannerContext from "../contexts/BannerContext";
-
-export default function useBanner(){
-    return useContext(BannerContext);
-}
--- src/hooks/useData.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import DataContext from "../contexts/DataContext";
-
-export default function useData(){
-    return useContext(DataContext);
-}
